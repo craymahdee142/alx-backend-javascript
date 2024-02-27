59e58f9678d937b1450500bcc735f500b98fcd50
Daniel Craymah <craymahdaniel@gmail.com>
2024-02-27 21:55:28 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Prints the given message to STDOUT.
+ *
+ * @param {msg} str - The message to be printed.
+ */
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
